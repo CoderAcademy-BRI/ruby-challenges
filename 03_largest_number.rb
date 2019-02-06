500f247a5f2f33ecd5bae0221891ca2ce2df8c24
RyanG91 <ryan.grosser91@gmail.com>
2019-02-06 14:02:57 +1000

    Largest number challenge added

---
+++ 03_largest_number.rb
@@ -0,0 +1,19 @@
+# Largest Number
+#
+# Write a method that will take two numbers,
+# and return whichever is the largest.
+#
+# Example:
+# largest_number(100,50) should return 100
+# largest_number(10,20) should return 20
+#
+# Check your solution by filling in the folowing table:
+# number_1 || number_2 || expected || actual
+# 
+# If your stuck feel free to work togeather
+
+def largest_number (number_1, number_2)
+  # Your code here
+end
+
+largest_number(100,50)
