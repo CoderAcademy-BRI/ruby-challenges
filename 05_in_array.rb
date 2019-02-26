a600065b3f320365a06fe044bcddc70911b1fe59
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-02-26 17:35:47 +1000

    In array challenge added (#5)

---
+++ 05_in_array.rb
@@ -0,0 +1,23 @@
+# Methods
+
+# Write a method called 'in_array' which will take 
+# two parameteres:
+# One as a string and one as an array of strings
+
+# Return a boolean indicationg whether or not the
+# string is found in the array.
+
+# Test your solution with:
+# ruby tests/05_in_array_test.rb
+
+# Example:
+# "hello", ["hi","howdy","hello"] should return true.
+
+def in_array (needle, haystack)
+  # Your code here
+end
+
+
+# Beast Mode:
+
+# Redo the challenge but try solving it again in a different way.
