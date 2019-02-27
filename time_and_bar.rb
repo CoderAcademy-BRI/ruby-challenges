2171cb55dc66a4ddea2c1d5471b47e4494dddd7b
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-02-28 06:50:00 +1000

    Added time and bar challenge (#7)

---
+++ time_and_bar.rb
@@ -0,0 +1,29 @@
+# There are three parts to this challenge and remember to push your solution.
+
+# PART 1:
+# - Create a well named variable that contains the amount of time it took you to get to class
+# - Create a complete sentence  that lets us know how you got to class and how long it took
+# - Print this complete sentence
+
+
+# PART 2:
+# You are working at a bar where you have a current backlog of drinks to make:
+# 3 cocktails
+# 2 waters
+# and
+# 6 beers
+
+# Write a program that asks the customer for their order.
+#   if they order a cocktail, add one to the number of cocktails you need to make,
+#   if they order a water, add one to the number of waters you need to make,
+#   if they order a beer, add one to the number of beers you need to pour
+
+# Print the final drinks order so you know what to make
+
+
+# Part Three:
+# Cocktails sell for $22, and cost $8 to make
+# Beer sell for $12, and cost $3 to pour
+# Water sell for $6, and cost $0.15 to make
+
+# Print out the total profit for the orders you have
