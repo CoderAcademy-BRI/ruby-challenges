7db251037940e1eef793cf520e49867005ffaee3
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-03-05 06:49:10 +1000

    Added count letters challenge (#10)

---
+++ 08_count_letters.rb
@@ -0,0 +1,17 @@
+# Count Letters
+
+# Write a method that will take a string, keep count
+# of each letter and return the totals as a hash.
+
+# Example:
+# count_letters("hello") should return {"h"=>1, "e"=>1, "l"=>2, "o"=>1}
+# count_letters("mississippi") should return {"m"=>1, "i"=>4, "s"=>4, "p"=>2}
+
+# Check your solution by running the tests:
+# ruby tests/08_count_letters_test.rb
+
+def count_letters (string)
+  result = {} # You'll need an empty hash to get started!
+
+  return result # return the hash
+end
