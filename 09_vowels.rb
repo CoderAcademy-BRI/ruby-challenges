edb6dc4f8813f7619f327824eb92afe925d068d0
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-03-05 17:27:44 +1000

    Vowels challenge added (#11)

---
+++ 09_vowels.rb
@@ -0,0 +1,15 @@
+# Vowels
+
+# Write a method that will take a string and
+# return an array of vowels used in that string.
+
+# Example:
+# count_vowels("The quick brown fox") should return ["e","u","i","o","o"]
+# count_vowels("Hello World") should return ["e","o","o"]
+
+# Check your solution by running the tests:
+# ruby tests/09_vowels_test.rb
+
+def vowels (string)
+  # Your code here
+end
