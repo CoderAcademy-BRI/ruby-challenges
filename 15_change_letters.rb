8bedbac6d45c7bd64aeea2b697014541499015ed
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-03-07 11:28:50 +1000

    Change letters challenge added (#17)

---
+++ 15_change_letters.rb
@@ -0,0 +1,21 @@
+# Write a program which will ask users for a string input.
+# The program should display the input unless, it contains the letter 's'.
+
+# If 's' is included change the output so that it will be 'th' instead.
+
+# Examples:
+# input = say hi
+# output = thay hi
+
+# input = mississippi
+# output = miththiththippi
+
+# Hint:
+# Look up String on Ruby docs to help.
+
+# Check your solution by running:
+# ruby 15_change_letters.rb
+
+
+
+# Your code here
