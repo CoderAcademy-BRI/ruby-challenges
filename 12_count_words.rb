937f429a6570923b9196f3610cb404d51c87a14a
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-03-07 11:08:03 +1000

    Count words challenge added (#14)

---
+++ 12_count_words.rb
@@ -0,0 +1,22 @@
+# Your challenge is to write a program which will ask the user for input.
+# The code should then output the number of times words are used.
+
+# Example:
+# If the words 'hi hi Hi hello 1 1' are input, the result should be:
+# hi 2
+# Hi 1
+# hello 1
+# 1 2
+
+# Test your solution by running 12_count_words
+
+
+# Your code here
+
+
+# Beast Mode:
+# Change your code so that it doesn't matter if capital letters are used,
+# In the above example the result should be:
+# hi 3
+# hello 1
+# 1 2
