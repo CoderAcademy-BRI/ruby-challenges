adb499cafc46a70114f2fb2e4262f3897a4ccaf9
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-03-07 11:27:56 +1000

    Hackerman challenge added (#16)

---
+++ 14_hackerman.rb
@@ -0,0 +1,26 @@
+# You have to stop the infamous hackerman by writing a basic security program.
+
+# Your task is to write a program which will ask the user for input twice.
+# Both times should ask the user to type words and will display the FIRST input.
+
+# However if any of the words in the second input match with a word from the
+# first input it should say "HIDDEN" instead of the matched word.
+
+# Example:
+# first_input = bob rob dob mob
+# second_input = kob fob dob
+
+# output = bob rob HIDDEN mob
+
+# Check your solution by running:
+# ruby 14_hackerman.rb
+
+
+
+# Your code here
+
+
+
+# Beast mode:
+# Does your code deal with capital letters? Update your solution so that
+# they will be downsized if they are input but HIDDEN remains capitalised.
