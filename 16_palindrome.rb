c11bc6e2f1abf17db751837261728dea38b44e1e
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-03-11 12:09:48 +1000

    Palindrome challenge added (#18)

---
+++ 16_palindrome.rb
@@ -0,0 +1,21 @@
+# Palindrome
+
+# Add tests to the 16_palindrome_test.rb file, inside the test folder.
+# The test will check your method takes a word as a string and return 
+# a boolean indicating whether or not the word is a palindrome.
+
+# If your having trouble writing tests, look over previous morning challenges.
+
+# Once you have done this, try to write the method below and check
+# your code by running your tests.
+
+# Example:
+# palindrome("hello") should return false
+# palindrome("racecar") should return true
+
+# Check your solution by running the tests:
+# ruby tests/16_palindrome_test.rb
+
+def palindrome (string)
+  # Your code here
+end
