bbfdad0511b40c0003dfc3256a1892fbc4555b44
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-03-11 12:24:44 +1000

    more description and test for strings

--- 17_combine_hashes.rb
+++ 17_combine_hashes.rb
@@ -3,11 +3,23 @@
 # Write a method which will take two different hashes and will
 # return their combined value in a new hash.
 
+# If the hash contains numbers, the numbers for matching keys should
+# be added and the sum stored in the combined hash
+
+# If the hash contains strings, the strings for matching keys should
+# be concatenated and the result stored in the combined hash
+
 # Example:
 # hash1 = { a: 3, b: 5, c: 1}
 # hash2 = { a: 5, b: 2, c: 14}
-
 # combine_hashes => { a: 8, b: 7, c: 15 }
+
+# Example: 
+# hash1 = {a: "a", b:"b", c:"c"}
+# hash2 = {a: "a", b:"b", c:"c"}
+# combine_hashes => {a: "aa", b:"bb", c:"cc"}
+
+
 
 # Test your solution with ruby tests/17_combine_hashes.rb
 
--- tests/17_combine_hashes_test.rb
+++ tests/17_combine_hashes_test.rb
@@ -2,7 +2,10 @@ require 'test/unit'
 require_relative '../17_combine_hashes'
 
 class CombineHashesTest < Test::Unit::TestCase
-  def test_combine_hashes
+  def test_combine_hashes_num
     assert_equal({a: 8, b: 7, c: 14}, combine_hashes({a: 3, b: 5, c: 1}, {a: 5, b: 2, c: 13}))
+  end
+  def test_combine_hashes_string
+    assert_equal({a: "aa", b: "bb", c: "cc"}, combine_hashes({a: "a", b: "b", c: "c"}, {a: "a", b: "b", c: "c"}))
   end
 end
