81e46faea3bf8de55ef764c4abc4d6c440b9bae5
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-03-19 10:42:20 +1000

    added 20_descending_numbers

---
+++ 20_descending_numbers.rb
@@ -0,0 +1,25 @@
+# Background
+# Using array and string methods
+# Passing in one argument
+
+## Specs
+
+#Your task is to make a function that can take any 
+#non-negative integer as a argument and return it 
+#with its digits in descending order. 
+#Essentially, rearrange the digits to create 
+#the highest possible number.
+
+#Examples:
+#Input: 21445 Output: 54421
+#Input: 145263 Output: 654321
+#Input: 1254859723 Output: 9875543221
+
+def descending_order(n)
+    # Your code goes here
+end
+
+# Test your code here
+descending_order(1)
+
+# Add some interesting test cases that test edge cases
