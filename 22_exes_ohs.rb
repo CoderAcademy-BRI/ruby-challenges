85cbb7c5afd0b20a011ceed2d9e803213fd2f716
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-03-24 20:14:48 +1000

    added exes_ohs

---
+++ 22_exes_ohs.rb
@@ -0,0 +1,37 @@
+# Exes and Ohs
+# Difficulty: medium/hard
+
+# This is a great question for interview prep, so lets not google it. 
+# Instead, try whiteboarding, and thinking of your approach before coding. There are multiple approaches to this question, so try to think of the most efficient one you can!
+
+# Write a method that will take a string as an input. 
+# The method will check this string, and count the amount 
+# of 'x's and 'o's. It should return true if the amount of 'x's 
+# and 'o's are the same, and should return false if they are different. 
+# It should also be case insensitive, and work with any set of two 
+# characters (for example, the string could contain "oooxxx" or
+# "eeefff" - both of these would return true because there are
+# two distinct characters and an equal number of each in both
+# examples).
+
+# Example input: "ooxx"
+# Expected output: true
+
+# Example input: "oOxXxoX"
+# Expected output: false
+
+# Example input: "ooXx"
+# Expected output: true
+
+# Example input: "rtrt"
+# Expected output: true
+
+# Example input: "RtrT"
+# Expected output: true
+
+# Example input: "efefy"
+# Expected output: false
+
+def exes_and_ohs(input)
+    # Your code goes here
+end
---
+++ tests/22_exes_ohs_test.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require_relative '../22_exes_ohs'
+
+class ExesOhsTest < Test::Unit::TestCase
+  def test_simple_two_chars_same_case
+    assert_equal(true, exes_and_ohs("ooxx"))
+    assert_equal(false, exes_and_ohs("oox"))
+  end
+  def test_simple_two_chars_diff_case
+    assert_equal(true, exes_and_ohs("oOxx"))
+    assert_equal(false, exes_and_ohs("oOx"))
+  end
+  def test_medium_two_chars_not_xo
+    assert_equal(true, exes_and_ohs("eeff"))
+    assert_equal(false, exes_and_ohs("eef"))
+  end
+  def test_medium_two_chars_not_xo_unmatched_case
+    assert_equal(true, exes_and_ohs("eEFf"))
+    assert_equal(false, exes_and_ohs("eEf"))
+  end
+  def test_hard_more_than_two_chars
+    assert_equal(true, exes_and_ohs("ooxxe"))
+    assert_equal(false, exes_and_ohs("abc"))
+  end
+end
