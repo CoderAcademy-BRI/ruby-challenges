d803e12e3b11627a2fdc70c32df1d4ca01cf3571
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-03-24 20:30:43 +1000

    added isogram

---
+++ 24_isogram.rb
@@ -0,0 +1,23 @@
+# Isogram
+# Difficulty: medium
+
+# An isogram is a word that has no repeatting letters; 
+# consecutive or non-consecutive.
+
+# Implement a method that determines whether a string 
+# that contains only letters is an isogram. 
+# The function will take the string as input, and will 
+# return either true or false.
+
+
+# *E.g.* isogram?("hello") should return false
+
+# *E.g.* isogram?("scary") should return true
+
+def isogram?(input)
+    # Your code goes here
+end
+
+# Test your code here
+puts isogram?("hello") #should return false
+puts isogram?("scary") #should return true
