1e2293208970241bef7e9a73fb0eba266984bb08
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-03-29 07:37:59 +1000

    added 26_max_min

---
+++ 26_max_min.rb
@@ -0,0 +1,21 @@
+# Max Min In String
+
+# There are plenty of incredible array methods built into 
+# ruby that allow us to compare and run calculations on 
+# arrays full of numbers. Sometimes, we don't receive these 
+# numbers in an array, and instead receive it as a string of 
+# space seperated numbers.
+
+# Write a method that will take a string of space seperated 
+# numbers as input, and return the highest and lowest number 
+# from this string.
+
+# *E.g.* max_min_in_string("5 2 9 8 23 1") should return "23 1"
+# *E.g.* max_min_in_string("15 4 4 38 -9") should return "38 -9"
+
+# Write your own test file for this challenge using Test::Unit
+
+
+def max_min(num_string)
+    # your code goes here
+end
